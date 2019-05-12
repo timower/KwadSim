1587ca43dadc3662086bf34f28bf7c77a4225e3b
Timothy Werquin <timothy@werquin.com>
2019-05-12 14:29:53 +0200

    Fix osx build scripts

--- .travis.sh
+++ .travis.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-cd Script/native
-cd godot-cpp
-scons bits=64 platform=linux generate_bindings=yes || exit $?
-scons bits=64 platform=windows || exit $?
-cd ..
-scons platform=linux || exit $?
-scons platform=windows || exit $?
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    cd Script/native
+    cd godot-cpp
+    scons bits=64 platform=osx generate_bindings=yes || exit $?
+    cd ..
+    scons platform=osx use_llvm=yes || exit $?
+else
+    cd Script/native
+    cd godot-cpp
+    scons bits=64 platform=linux generate_bindings=yes || exit $?
+    scons bits=64 platform=windows || exit $?
+    cd ..
+    scons platform=linux || exit $?
+    scons platform=windows || exit $?
+fi
